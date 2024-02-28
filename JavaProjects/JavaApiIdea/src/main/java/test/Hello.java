73b11caf9dfffccf6b0fd83889764c45581d8222
George Sayapin <prorabserv@gmail.com>
2024-02-27 19:06:28 -0500

    created apis

---
+++ JavaProjects/JavaApiIdea/src/main/java/test/Hello.java
@@ -0,0 +1,37 @@
+package test;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@Path("/hello")
+public class Hello {
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayHelloXML()
+    {
+        String resource="<? xml version='1.0' ?>" +
+                "<hello> Hello from XML</hello>";
+        return resource;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String sayHelloJSON()
+    {
+        String resource="Hello from JSON";
+        return resource;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHelloHTML()
+    {
+        String resource="<h1>Hello from HTML</h1>";
+        return resource;
+    }
+}
